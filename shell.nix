215cc0b9d562d5b9b0aaa3f6eaba8d819237b195
Derek Maffett <derekgmaffett@gmail.com>
2019-08-30 16:17:56 +0200

    remove out

--- shell.nix
+++ shell.nix
@@ -8,6 +8,5 @@ pkgs.mkShell {
     (pkgs.writeScriptBin "run" ".slides/$(tmux display-message -p '#W')")
     (pkgs.writeScriptBin "present" "create-interactive-slideshow ./slides.hs && tmuxinator start -p ./.slides/.slideshow.yml")
     (pkgs.writeScriptBin "watch" "fswatch -r -o -l 0.2 ./.slides/$1.hs | (while read; do stack ghc ./.slides/$1.hs; done)")
-    (pkgs.writeScriptBin "out" "tk && os")
   ];
 }
